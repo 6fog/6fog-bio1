14a627fb02145e98d330fef3dc0609c48771a872
6fog <0joeberg@gmail.com>
2025-08-22 23:44:11 +0200

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,5 +1,5 @@
         // Texto que será exibido no <title>
-        const titleText = "@.6fog";
+        const titleText = "@6fog";
         let index = 0;
 
         // Função para animar o título
